1a5ac629540cdbc3930ae78157be72109274f761
tonya <antonina.cherednichenko@gmail.com>
2016-01-27 15:37:55 +0200

    Changed support of function / variable names + added parameters processing

--- toolchain-clojure/src/main/java/com/sourcegraph/toolchain/clojure/ClojureParseTreeListener.java
+++ toolchain-clojure/src/main/java/com/sourcegraph/toolchain/clojure/ClojureParseTreeListener.java
@@ -9,9 +9,12 @@ import com.sourcegraph.toolchain.core.objects.Ref;
 import com.sourcegraph.toolchain.language.Context;
 import com.sourcegraph.toolchain.language.LookupResult;
 import com.sourcegraph.toolchain.language.Scope;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 
 class ClojureParseTreeListener extends ClojureBaseListener {
@@ -23,8 +26,7 @@ class ClojureParseTreeListener extends ClojureBaseListener {
 
     Context<Boolean> context = new Context<>();
 
-    //private boolean isInFunctionDef;
-
+    private Map<ParserRuleContext, Boolean> defs = new IdentityHashMap<>();
 
     ClojureParseTreeListener(LanguageImpl support) {
         this.support = support;
@@ -32,14 +34,6 @@ class ClojureParseTreeListener extends ClojureBaseListener {
 
     @Override
     public void enterFunction_def(ClojureParser.Function_defContext ctx) {
-        ClojureParser.Fn_nameContext nameCtx = ctx.fn_name();
-        context.enterScope(new Scope<Boolean>(nameCtx.getText())); // params here
-    }
-
-    @Override
-    public void exitFunction_def(ClojureParser.Function_defContext ctx) {
-        context.exitScope();
-
         ClojureParser.Fn_nameContext nameCtx = ctx.fn_name();
         String fnStartKeyword = ctx.fn_start().getText();
 
@@ -50,25 +44,30 @@ class ClojureParseTreeListener extends ClojureBaseListener {
         emit(fnDef, context.currentScope().getPathTo(fnDef.name, PATH_SEPARATOR));
 
         context.currentScope().put(fnDef.name, true);
+        defs.put(ctx.fn_name().symbol(), true);
 
+        context.enterScope(new Scope<>(nameCtx.getText(), context.currentScope().getPrefix()));
+
+        List<ClojureParser.ParameterContext> params = ctx.arguments().parameter();
+        for (ClojureParser.ParameterContext param : params) {
+            Def paramDef = support.def(param, DefKind.PARAM);
+            paramDef.format("param", StringUtils.EMPTY, DefData.SEPARATOR_EMPTY);
+            paramDef.defData.setKind("param");
+
+            emit(paramDef, context.currentScope().getPathTo(paramDef.name, PATH_SEPARATOR));
+
+            context.currentScope().put(param.parameter_name().getText(), true);
+            defs.put(param.parameter_name().symbol(), true);
+        }
+    }
+
+    @Override
+    public void exitFunction_def(ClojureParser.Function_defContext ctx) {
+        context.exitScope();
     }
 
     @Override
     public void enterVar_def(ClojureParser.Var_defContext ctx) {
-//        ClojureParser.Var_nameContext nameCtx = ctx.var_name();
-//        String varStartKeyword = ctx.var_start().getText();
-//
-//        Def varDef = support.def(nameCtx, DefKind.VAR);
-//        varDef.format(varStartKeyword, StringUtils.EMPTY, DefData.SEPARATOR_EMPTY);
-//        varDef.defData.setKind(varStartKeyword);
-//
-//        emit(varDef, context.currentScope().getPathTo(varDef.name, PATH_SEPARATOR));
-//
-//        context.currentScope().put(varDef.name, true);
-    }
-
-    @Override
-    public void exitVar_def(ClojureParser.Var_defContext ctx) {
         ClojureParser.Var_nameContext nameCtx = ctx.var_name();
         String varStartKeyword = ctx.var_start().getText();
 
@@ -79,11 +78,16 @@ class ClojureParseTreeListener extends ClojureBaseListener {
         emit(varDef, context.currentScope().getPathTo(varDef.name, PATH_SEPARATOR));
 
         context.currentScope().put(varDef.name, true);
+        defs.put(ctx.var_name().symbol(), true);
     }
-
 
     @Override
     public void enterSymbol(ClojureParser.SymbolContext ctx) {
+        //check if it's name of some declaration
+        if (defs.containsKey(ctx)) {
+            return;
+        }
+
         String ident = ctx.getText();
         LookupResult result = context.lookup(ident);
         if (result == null) {
@@ -93,28 +97,6 @@ class ClojureParseTreeListener extends ClojureBaseListener {
         Ref ref = support.ref(ctx);
         emit(ref, result.getScope().getPathTo(ident, PATH_SEPARATOR));
     }
-
-//    @Override
-//    public void enterList(ClojureParser.ListContext ctx) {
-//        List<FormContext> listForms = ctx.forms().form();
-//        FormContext first = listForms.get(0);
-//        LOGGER.debug("Number of pass = " + String.valueOf((support.firstPass == true) ? 1 : 2));
-//        LOGGER.debug("Entered list with such first element = " + first.getText());
-//
-//        //check if we found call of function here
-//        String ident = first.getText();
-//        LookupResult result = context.lookup(ident);
-//        if (result == null) {
-//            return;
-//        }
-//
-//        Ref ref = support.ref(first);
-//        emit(ref, result.getScope().getPathTo(ident, PATH_SEPARATOR));
-//    }
-//
-//    @Override
-//    public void exitList(ClojureParser.ListContext ctx) {
-//    }
 
     private void emit(Def def, String path) {
         if (!support.firstPass) {
