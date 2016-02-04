a3d14ebd83b59af5c3286a81437423d11a892cc5
tonya <antonina.cherednichenko@gmail.com>
2016-02-04 14:19:33 +0200

    Readability fix of modifiers in the interface

--- toolchain-clojure/src/main/java/com/sourcegraph/toolchain/clojure/DefKind.java
+++ toolchain-clojure/src/main/java/com/sourcegraph/toolchain/clojure/DefKind.java
@@ -2,8 +2,8 @@ package com.sourcegraph.toolchain.clojure;
 
 interface DefKind {
 
-    public static final String FUNC = "func";
-    public static final String PARAM = "param";
-    public static final String BINDING_VAR = "binding_var";
-    public static final String VAR = "var";
+    String FUNC = "func";
+    String PARAM = "param";
+    String BINDING_VAR = "binding_var";
+    String VAR = "var";
 }
